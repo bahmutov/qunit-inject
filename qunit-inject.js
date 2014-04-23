292ea1e56bac45e4f2286ad533f393a588d8cc30
Gleb Bahmutov <gleb@kensho.com>
2014-04-22 23:31:31 -0400

    working initial implementation

--- qunit-inject.js
+++ qunit-inject.js
@@ -1,98 +1,55 @@
 /* jshint -W117:false */
 (function (QUnit, env) {
-  if (env.__quit_once_initialized) {
+  if (env.__qunit_inject_initialized) {
     return;
   }
-  env.__quit_once_initialized = true;
+  env.__qunit_inject_initialized = true;
 
   if (typeof QUnit !== 'object') {
     throw new Error('undefined QUnit object');
+  }
+  var heroin = env.heroin;
+  if (typeof require === 'function') {
+    heroin = require('heroin');
+  }
+  if (typeof heroin !== 'function') {
+    throw new Error('No heroin to inject');
   }
 
   var _module = QUnit.module;
   if (typeof _module !== 'function') {
     throw new Error('QUnit.module should be a function');
   }
+  var _test = QUnit.test;
+  if (typeof _test !== 'function') {
+    throw new Error('QUnit.test should be a function');
+  }
+
+  var configs = {};
+  var lastModuleName;
 
   QUnit.module = function (name, config) {
     if (typeof config !== 'object') {
-      return;
+      return _module.call(QUnit, name, config);
+    }
+    configs[name] = config;
+    _module.call(QUnit, name, config);
+    lastModuleName = name;
+  };
+
+  QUnit.test = function (name, fn) {
+    if (typeof name === 'function') {
+      fn = name;
+      name = fn.name;
     }
 
-    (function addSetupOnce() {
-      if (QUnit.supports &&
-        QUnit.supports.setupOnce) {
-        return;
-      }
-
-      if (typeof config.setupOnce === 'function') {
-        var _setupOnceRan = false;
-        var _setup = typeof config.setup === 'function' ?
-          config.setup : null;
-
-        config.setup = function () {
-          if (!_setupOnceRan) {
-            config.setupOnce();
-            _setupOnceRan = true;
-          }
-
-          if (_setup) {
-            _setup.call(config);
-          }
-        };
-      }
-    }());
-
-    (function addTeardownOnce() {
-
-      if (QUnit.supports &&
-        QUnit.supports.teardownOnce) {
-        return;
-      }
-
-      function isLastTestInModule() {
-        if (QUnit.config && Array.isArray(QUnit.config.queue)) {
-          return QUnit.config.queue.length === 1;
-        } else {
-          // we cannot determine if the test is the last one in this module
-          return false;
-        }
-      }
-
-      if (typeof config.teardownOnce === 'function') {
-        var _teardown = typeof config.teardown === 'function' ?
-          config.teardown : null;
-
-        config.teardown = function () {
-          if (_teardown) {
-            _teardown.call(config);
-          }
-
-          if (isLastTestInModule()) {
-            config.teardownOnce();
-            config.teardownOnceRan = true;
-          }
-        };
-
-        // if multiple modules are used, the latest qunit
-        // puts everything into single queue. Figure out if
-        // current module is done
-        QUnit.moduleDone(function (details) {
-          // console.log('from', QUnit.config.currentModule);
-          // console.log('module done', details);
-
-          if (details.name === name) {
-            if (!config.teardownOnceRan) {
-              // console.log('running module teardown once');
-              config.teardownOnce();
-              config.teardownOnceRan = true;
-            }
-          }
-        });
-      }
-    }());
-
-    _module.call(QUnit, name, config);
+    if (lastModuleName && configs[lastModuleName]) {
+      var dependencies = configs[lastModuleName];
+      var injected = heroin(fn, dependencies);
+      _test.call(QUnit, name, injected);
+    } else {
+      _test.call(QUnit, name, fn);
+    }
   };
 }(QUnit, typeof global === 'object' ? global : window));
 
--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,14 @@
+QUnit.test('test without module', function () {
+  QUnit.ok(true, 'test runs');
+});
+
 QUnit.module('inject two variable', {
   a: 10,
   b: 32
+});
+
+QUnit.test('no injection', function () {
+  QUnit.ok(true, 'test runs');
 });
 
 QUnit.test('injected values', function (a, b) {
