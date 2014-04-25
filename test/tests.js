85c0fa75ae1a15f96341ee9bc7ba048f7db89265
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2014-04-25 13:47:57 -0400

    started work on setting injected values in setup method

--- test/tests.js
+++ test/tests.js
@@ -15,3 +15,15 @@ QUnit.test('injected values', function (a, b) {
   QUnit.equal(a, 10, 'valua a injected');
   QUnit.equal(b, 32, 'valua b injected');
 });
+
+QUnit.module('inject with setup', {
+  a: 10,
+  setup: function () {
+    this.a = 42;
+  }
+});
+
+/*
+QUnit.test('injected from setup', function (a) {
+  QUnit.equal(a, 42, 'value was changed by module setup');
+});*/
